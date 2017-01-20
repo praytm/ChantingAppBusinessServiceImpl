ecb1d48aa14f7bf0d9aae84fc5208c39661b8118
aditya_anand <aditya_anand>
2017-01-20 20:48:47 +0530

    tx changes

--- src/main/java/org/iskcon/nvcc/chantingApp/bs/impl/UserServiceImpl.java
+++ src/main/java/org/iskcon/nvcc/chantingApp/bs/impl/UserServiceImpl.java
@@ -7,7 +7,12 @@ import java.util.Date;
 
 import org.iskcon.nvcc.chantingApp.bs.UserService;
 import org.iskcon.nvcc.chantingApp.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.journaldev.dao.PersonDAO;
+import com.journaldev.model.Person;
 
 /**
  * @author aditya.anand
@@ -15,15 +20,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private PersonDAO dao;
 
 	/* (non-Javadoc)
 	 * @see org.iskcon.nvcc.chantingApp.bs.UserService#registerUser(org.iskcon.nvcc.chantingApp.dto.UserDTO)
 	 */
+	@Transactional
 	public UserDTO registerUser(UserDTO userDto) {
 		UserDTO user = new UserDTO();
 		user.setUserId(userDto.getUserId());
 		user.setName(userDto.getName() + "through service");
 		user.setCreatedDate(new Date());
+		Person person = new Person();
+		person.setName(user.getName());
+		dao.saveTxCheck();
+		dao.save(person);
 		return user;
 	}
 
