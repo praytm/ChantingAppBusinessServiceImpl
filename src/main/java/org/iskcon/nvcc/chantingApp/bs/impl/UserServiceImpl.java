c773a1832daadc8ce748fc7c475b34f9de29a325
er_aditya_anand <er.aditya.anand@gmail.com>
2017-01-22 22:23:39 +0530

    changes with loginservice

--- src/main/java/org/iskcon/nvcc/chantingApp/bs/impl/UserServiceImpl.java
+++ src/main/java/org/iskcon/nvcc/chantingApp/bs/impl/UserServiceImpl.java
@@ -6,13 +6,15 @@ package org.iskcon.nvcc.chantingApp.bs.impl;
 import java.util.Date;
 
 import org.iskcon.nvcc.chantingApp.bs.UserService;
+import org.iskcon.nvcc.chantingApp.bs.mapper.UserDTOMapper;
+import org.iskcon.nvcc.chantingApp.dao.LoginDAO;
+import org.iskcon.nvcc.chantingApp.dao.RegistrationDAO;
+import org.iskcon.nvcc.chantingApp.dao.User;
+import org.iskcon.nvcc.chantingApp.dao.UserStatus;
 import org.iskcon.nvcc.chantingApp.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.journaldev.dao.PersonDAO;
-import com.journaldev.model.Person;
 
 /**
  * @author aditya.anand
@@ -20,24 +22,42 @@ import com.journaldev.model.Person;
  */
 @Service
 public class UserServiceImpl implements UserService {
-	
-	@Autowired
-	private PersonDAO dao;
 
-	/* (non-Javadoc)
-	 * @see org.iskcon.nvcc.chantingApp.bs.UserService#registerUser(org.iskcon.nvcc.chantingApp.dto.UserDTO)
-	 */
+	@Autowired
+	private RegistrationDAO registrationDao;
+
+	@Autowired
+	private LoginDAO loginDAO;
+
 	@Transactional
 	public UserDTO registerUser(UserDTO userDto) {
-		UserDTO user = new UserDTO();
-		user.setUserId(userDto.getUserId());
-		user.setName(userDto.getName() + "through service");
-		user.setCreatedDate(new Date());
-		Person person = new Person();
-		person.setName(user.getName());
-		dao.saveTxCheck();
-		dao.save(person);
-		return user;
+		User user = UserDTOMapper.getUser(userDto, UserStatus.REGISTERED);
+		User savedUser = registrationDao.registerUser(user);
+		return UserDTOMapper.getUserDTO(savedUser);
+
+	}
+
+	@Transactional
+	public UserDTO loginUser(UserDTO userDto) {
+		UserDTO outputUserDto;
+		User userInput = UserDTOMapper.getUser(userDto, null);
+		User userOutput = loginDAO.loginUser(userInput);
+		if (null == userOutput) {
+			// matching record found corresponding to email id ,but password did
+			// not match
+			return null;
+		}
+		if (null != userOutput.getUserId()) {
+			outputUserDto = UserDTOMapper.getUserDTO(userOutput);
+			// it means there is a successful login and user instance is coming
+			// from db, so update the last login time to current time
+			userOutput.setLastLoginDate(new Date());
+			loginDAO.updateUser(userOutput);
+		} else {
+			//no matching record found in db corresponding to email used for login
+			outputUserDto = UserDTOMapper.getUserDTO(userOutput);
+		}
+		return outputUserDto;
 	}
 
 }
---
+++ src/main/java/org/iskcon/nvcc/chantingApp/bs/mapper/UserDTOMapper.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package org.iskcon.nvcc.chantingApp.bs.mapper;
+
+import java.util.Date;
+
+import org.iskcon.nvcc.chantingApp.dao.User;
+import org.iskcon.nvcc.chantingApp.dao.UserStatus;
+import org.iskcon.nvcc.chantingApp.dto.UserDTO;
+
+/**
+ * @author MA041SH
+ *
+ */
+public class UserDTOMapper {
+
+	
+	/**
+	 * To map User to UserDTO without mapping password
+	 * @param user
+	 * @return
+	 */
+	public static UserDTO getUserDTO(User user){
+		if(null != user){
+			UserDTO userDTO = new UserDTO();
+			userDTO.setCreatedDate(user.getCreationDate());
+			userDTO.setEmail(user.getEmail());
+			userDTO.setMobile(user.getMobile());
+			userDTO.setName(user.getName());
+			userDTO.setLastLoginDate(user.getLastLoginDate());
+			if(null != user.getUserId()){
+				userDTO.setUserId(user.getUserId().toString());	
+			}			
+			return userDTO;
+		}
+		return null;
+	}
+	
+	/**
+	 * To map UserDTO to User with option of setting UserStatus from allowed enum values
+	 * @param userDto
+	 * @param status
+	 * @return
+	 */
+	public static User getUser(UserDTO userDto, UserStatus status){
+		if(null != userDto){
+			User user = new User();
+			user.setCreationDate(new Date());
+			user.setEmail(userDto.getEmail());
+			user.setMobile(userDto.getMobile());
+			user.setPassword(userDto.getPassword());
+			user.setName(userDto.getName());
+			if(null != status){
+				user.setUserStatus(status.toString());	
+			}		
+			return user;	
+		}
+		return null;
+	}
+}
